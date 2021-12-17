fa101d03c76325771153e3b1e004d12e188c1861
Jack Kufa <jack@kufa.io>
2021-12-16 20:33:04 -0600

    FIX CODE (needed to deep copy R)

--- learner3.py
+++ learner3.py
@@ -3,64 +3,60 @@
 # a valid cell is a reachable cell (blank cell)
 
 from random import choice
+from copy import deepcopy
 
-DIMENSIONS = [6,8]
+DIMENSIONS = [6, 8]
 GAMMA = 0.8  # Discount rate
-N = 10  # Number of iterations
+N = 100000  # Number of iterations
 INF = 9999999
 
 # Mapping of all rewards
-REWARDS = {
-  (1, 3): 3, #🍩
-  (1, 6): -10, #👹
-  (3, 3): -5, #🔥
-  (3, 6): 10 #🍰
-}
+REWARDS = {(1, 3): 3, (1, 6): -10, (3, 3): -5, (3, 6): 10}  # 🍩  # 👹  # 🔥  # 🍰
 
 # Dictionary of all possible actions
 ACTIONS = {
-  (1, 1): ("v"),
-  (1, 4): (">"),
-  (1, 5): ("v", "<"),
-  (2, 1): ("^", "v", ">"),
-  (2, 2): ("<", ">"),
-  (2, 3): ("<"),
-  (2, 5): ("^", "v"),
-  (3, 1): ("^", "v"),
-  (3, 5): ("^", "v"),
-  (4, 1): ("^", ">"),
-  (4, 2): ("<", ">"),
-  (4, 3): ("<", ">"),
-  (4, 4): ("<", ">"),
-  (4, 5): ("^", "<"),
+    (1, 1): ("v"),
+    (1, 4): ("<", ">"),
+    (1, 5): ("v", "<", ">"),
+    (2, 1): ("^", "v", ">"),
+    (2, 2): ("<", ">"),
+    (2, 3): ("^", "v", "<"),
+    (2, 5): ("^", "v"),
+    (3, 1): ("^", "v"),
+    (3, 5): ("^", "v", ">"),
+    (4, 1): ("^", ">"),
+    (4, 2): ("<", ">"),
+    (4, 3): ("^", "<", ">"),
+    (4, 4): ("<", ">"),
+    (4, 5): ("^", "<"),
 }
 
-#Define all states
-ALL_STATES=[]
+# Define all states
+ALL_STATES = []
 for i in range(6):
     for j in range(8):
-            ALL_STATES.append((i,j))
+        ALL_STATES.append((i, j))
 
 # Initialize V
-V={}
+V = {}
 for s in ALL_STATES:
     if s in ACTIONS.keys():
         V[s] = 0
     else:
-      V[s] = -1
+        V[s] = -1
     if s in REWARDS.keys():
         V[s] = REWARDS[s]
 
-R = V
+R = deepcopy(V)
 
-#Define initial policy
-POLICY={}
+# Define initial policy
+POLICY = {}
 for s in ACTIONS.keys():
     POLICY[s] = choice(ACTIONS[s])
 
 
 def prob(s, a, nxt):
-    p = .09
+    p = 0.09
     # determine if l_prime is the same direction as a and assign p to .82 if yes
     return p
 
@@ -71,97 +67,92 @@ def exp_reward(l, a):
     # res += prob(l, a, l_prime) * reward(l, a, l_prime)
     return res
 
+
 # TODO: finish exp_reward and prob functions, make sure v_next and v_prev are being set properly!
 def value_iteration():
     for i in range(N):
         for s in ALL_STATES:
             if s in POLICY:
-                new_v = 0
+                new_v = -INF
 
                 for a in ACTIONS[s]:
                     nxt_sum = 0
                     alt_a1 = s
                     alt_a2 = s
                     if a == "^":
-                        nxt = (s[0]-1, s[1])
-                        if "<" in ACTIONS[s]:
-                          alt_a1 = (s[0], s[1]-1) # L
-                        if ">" in ACTIONS[s]:
-                          alt_a2 = (s[0], s[1]+1) # R
-                    if a == "v":
-                        nxt = (s[0]+1, s[1])
-                        # if "<" in ACTIONS[s]:
-                        alt_a1 = (s[0], s[1]-1) # L
-                        # if ">" in ACTIONS[s]:
-                        alt_a2 = (s[0], s[1]+1) # R
-                    if a == "<":
-                        nxt = (s[0], s[1]-1)
-                        # if "v"in ACTIONS[s]:
-                        alt_a1 = (s[0]+1, s[1]) # L
-                        # if "^" in ACTIONS[s]:
-                        alt_a2 = (s[0]-1, s[1]) # R
-                    if a == ">":
-                        nxt = (s[0], s[1]+1)
-                        # if "v"in ACTIONS[s]:
-                        alt_a1 = (s[0]+1, s[1]) # L
-                        # if "^" in ACTIONS[s]:
-                        alt_a2 = (s[0]-1, s[1]) # R
-                    nxt_sum = (.82 * V[nxt]) + (.09 * V[alt_a1]) + (.09 * V[alt_a2])
-                    v = R[s] + GAMMA * nxt_sum
+                        nxt = (s[0] - 1, s[1])
+                        alt_a1 = (s[0], s[1] - 1)  # L
+                        alt_a2 = (s[0], s[1] + 1)  # R
+                    elif a == "v":
+                        nxt = (s[0] + 1, s[1])
+                        alt_a1 = (s[0], s[1] - 1)  # L
+                        alt_a2 = (s[0], s[1] + 1)  # R
+                    elif a == "<":
+                        nxt = (s[0], s[1] - 1)
+                        alt_a1 = (s[0] + 1, s[1])  # L
+                        alt_a2 = (s[0] - 1, s[1])  # R
+                    elif a == ">":
+                        nxt = (s[0], s[1] + 1)
+                        alt_a1 = (s[0] + 1, s[1])  # L
+                        alt_a2 = (s[0] - 1, s[1])  # R
+                    nxt_sum = (0.82 * V[nxt]) + (0.09 * V[alt_a1]) + (0.09 * V[alt_a2])
+                    v = R[s] + GAMMA * nxt_sum # R[s] is sus
 
                     if v > new_v:
                         new_v = v
                         POLICY[s] = a
                 V[s] = new_v
 
+
 def print_output():
-  r = "+--------+--------+--------+--------+--------+--------+"
-  values = []
-  actions = []
-  for s in V:
-    if s[0] != 0 and s[0] != 5 and s[1] != 0 and s[1] != 7:
-      if V[s] == -1:
-        values.append("XXXXXX")
-        actions.append("XXXXXX")
-      elif V[s] == 10:
-        values.append("CAKE")
-        actions.append("CAKE")
-      elif V[s] == 3:
-        values.append("DONUT")
-        actions.append("DONUT")
-      elif V[s] == -5:
-        values.append("FIRE")
-        actions.append("FIRE")
-      elif V[s] == -10:
-        values.append("ONI")
-        actions.append("ONI")
-      else:
-        values.append(str(V[s])[0:5])
-        actions.append(POLICY[s]+ "  ")
-  print('CS-5001: HW#3\nProgrammer: Jack Kufa\nDiscount Gamma = ' + str(GAMMA)+'\n\nValues after ' + str(N) + ' iterations:\n'+r)
-  for i in range(4):
-    line = "|"
-    for j in range(6):
-      entry = values[0].rjust(6, ' ')
-      line += " " + entry + " |"
-      values.pop(0)
-    print(line)
-    print(r)
+    r = "+--------+--------+--------+--------+--------+--------+"
+    values = []
+    actions = []
+    for s in V:
+        if s[0] != 0 and s[0] != 5 and s[1] != 0 and s[1] != 7:
+            if V[s] == -1:
+                values.append("XXXXXX")
+                actions.append("XXXXXX")
+            elif V[s] == 10:
+                values.append("CAKE")
+                actions.append("CAKE")
+            elif V[s] == 3:
+                values.append("DONUT")
+                actions.append("DONUT")
+            elif V[s] == -5:
+                values.append("FIRE")
+                actions.append("FIRE")
+            elif V[s] == -10:
+                values.append("ONI")
+                actions.append("ONI")
+            else:
+                values.append(str(V[s])[0:5])
+                actions.append(POLICY[s] + "  ")
+    print(
+        "CS-5001: HW#3\nProgrammer: Jack Kufa\nDiscount Gamma = "
+        + str(GAMMA)
+        + "\n\nValues after "
+        + str(N)
+        + " iterations:\n"
+        + r
+    )
+    for i in range(4):
+        line = "|"
+        for j in range(6):
+            entry = values[0].rjust(6, " ")
+            line += " " + entry + " |"
+            values.pop(0)
+        print(line)
+        print(r)
 
-  print('\nPolicy:\n'+r)
-  for i in range(4):
-    line = "|"
-    for j in range(6):
-      entry = actions[0].rjust(6, ' ')
-      line += " " + entry + " |"
-      actions.pop(0)
-    print(line+'\n'+r)
-
-
-
-
-def output_policy():
-  pass
+    print("\nPolicy:\n" + r)
+    for i in range(4):
+        line = "|"
+        for j in range(6):
+            entry = actions[0].rjust(6, " ")
+            line += " " + entry + " |"
+            actions.pop(0)
+        print(line + "\n" + r)
 
 value_iteration()
 print_output()
